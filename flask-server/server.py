680f645bed25b818d6cae3a2b1af6e3b79c08053
Anshul Jha <65700919+ajha7@users.noreply.github.com>
2022-03-22 15:14:54 -0700

    Added example method, returns blah blah

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def clients():
     return {"clients": ["Bob Jones", "Bobby123"]}
 
+
+def exampleMethod():
+    return {"blah blah "}
+
 if __name__ == "__main__":
     app.run(debug=True)
