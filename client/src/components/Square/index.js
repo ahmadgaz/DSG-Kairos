73f4cdc230d035fcae752c9173f85d867ac7f752
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-03-28 14:55:53 -0700

    Cleanup

--- client/src/components/Square/index.js
+++ client/src/components/Square/index.js
@@ -7,19 +7,12 @@ const Square = (props) => {
   const link = "https://www.sundayfriends.org/" + title.replace(/\s+/g, '-')
   return (
     <div class="square" tabIndex="1">
-      {/* div for the clickable square */}
       <a class="square-button" href={link} target="_self" tabIndex="0">
-        {/* div for the image box */}
         <div class="image-box">
           <Image title={title}/>
         </div>
-        {/* div for the text label */}
         <div class="text-label">
-          {/* div for the text */}
-          <div>
-            {/* title */}
-            <div class="title">{title.toUpperCase()}</div>
-          </div>
+          <div class="title">{title.toUpperCase()}</div>
         </div>
       </a>
     </div>
