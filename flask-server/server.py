d723f1355a634d49fae6c3e823cece41fc9489fd
Rha02 <romuzus08@gmail.com>
2022-04-14 17:47:28 -0700

    Updated Route to Get Most Active Users

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,6 +3,7 @@ import pymysql, sys
 import os
 import atexit
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -39,6 +40,52 @@ def exampleMethod():
                 clients[-1][columns[i]] = res[i]
     
     return str(clients)
+
+# Returns most active users using specifications in lines 48 to 53
+@app.route("/get-active-users")
+def getMostActiveUsers():
+
+    # Initialize specification variables
+    responseSize = 20
+    endDate = datetime.today()
+    # Get the date 31 days ago.
+    startDate = endDate - timedelta(days=31)
+
+    clientsOrderedByFreqDesc = []
+
+    with conn.cursor() as cur:
+        columns = []
+        sqlQuery = "SHOW COLUMNS FROM Clients;"
+        cur.execute(sqlQuery)
+        cols = cur.fetchall()
+        for col in cols:
+            columns.append(col[0])
+        columns.append("frequency")
+
+        sqlQuery = """
+                SELECT clients.*, freq.frequency FROM sunday_friends.Clients clients
+                INNER JOIN (
+                    SELECT client_id, count(client_id) frequency FROM sunday_friends.Client_Attendance
+                    WHERE event_id in (
+                        SELECT id FROM sunday_friends.Events
+                        WHERE date_created BETWEEN '{start}' AND '{end}'
+                    )
+                    GROUP BY client_id
+                ) freq ON clients.id = freq.client_id
+                ORDER BY frequency DESC
+                LIMIT {limit};
+            """.format(start=startDate, end=endDate, limit=responseSize)
+        
+        cur.execute(sqlQuery)
+        result = cur.fetchall()
+
+        for client in result:
+            c = {}
+            for i in range(len(columns)):
+                c[columns[i]] = client[i]
+            clientsOrderedByFreqDesc.append(c)
+
+    return str(clientsOrderedByFreqDesc)
 
 if __name__ == "__main__":
     try:
