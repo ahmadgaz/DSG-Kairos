93160e6c6471d98b4b15d9b7fead591113624747
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-04-29 13:10:41 -0700

    Set color for buttons

--- client/src/components/Tabbar/index.js
+++ client/src/components/Tabbar/index.js
@@ -3,11 +3,7 @@ import React, { useState } from 'react';
 
 function Tabbar(props) {
 
-    const [style, setStyle] = useState("butt");
-
     const onClick = (e) => {
-        e.target.style.color = "white";
-        e.target.style.backgroundColor = "rgb(50, 173, 230)";
     }
 
     return (
@@ -15,19 +11,19 @@ function Tabbar(props) {
             <nav id="doc-nav" class="doc-nav" role="navigation">
                 <ul class="side">
                     <li class="nav-overview" id="client-frequency">
-                        <button class={style} onClick={onClick}>Client frequency at events</button>
+                        <button onClick={onClick}>Client frequency at events</button>
                     </li>
                     <li class="nav-overview" id="active-users">
-                        <button class={style} onClick={onClick}>Most active users</button>
+                        <button onClick={onClick}>Most active users</button>
                     </li>
                     <li class="nav-overview" id="age-groups">
-                        <button class={style} onClick={onClick}>Age groups</button>
+                        <button onClick={onClick}>Age groups</button>
                     </li>
                     <li class="nav-overview" id="another-metric">
-                        <button class={style} onClick={onClick}>Another metric</button>
+                        <button onClick={onClick}>Another metric</button>
                     </li>
                     <li class="nav-overview" id="another-metric">
-                        <button class={style} onClick={onClick}>One more metric for fun</button>
+                        <button onClick={onClick}>One more metric for fun</button>
                     </li>
                 </ul>
             </nav>
