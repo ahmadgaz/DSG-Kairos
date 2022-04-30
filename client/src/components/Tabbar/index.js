2541419058d0ac4eac0f43463cac27eabdaf19aa
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-04-30 11:42:38 -0700

    Fix button highlighting

--- client/src/components/Tabbar/index.js
+++ client/src/components/Tabbar/index.js
@@ -4,26 +4,30 @@ import React, { useState } from 'react';
 function Tabbar(props) {
 
     const onClick = (e) => {
+        document.querySelectorAll('button').forEach(btn => {
+            btn.classList.remove('active');
+        })
+        document.getElementById(e.target.id).classList.add('active');
     }
 
     return (
         <div class="tab-bar">
             <nav id="doc-nav" class="doc-nav" role="navigation">
                 <ul class="side">
-                    <li class="nav-overview" id="client-frequency">
-                        <button onClick={onClick}>Client frequency at events</button>
+                    <li class="nav-overview">
+                        <button class="btn" id="client-frequency" onClick={onClick}>Client frequency at events</button>
                     </li>
-                    <li class="nav-overview" id="active-users">
-                        <button onClick={onClick}>Most active users</button>
+                    <li class="nav-overview">
+                        <button class="btn" id="active-users" onClick={onClick}>Most active users</button>
                     </li>
-                    <li class="nav-overview" id="age-groups">
-                        <button onClick={onClick}>Age groups</button>
+                    <li class="nav-overview">
+                        <button class="btn" id="age-groups" onClick={onClick}>Age groups</button>
                     </li>
-                    <li class="nav-overview" id="another-metric">
-                        <button onClick={onClick}>Another metric</button>
+                    <li class="nav-overview">
+                        <button class="btn" id="another-metric" onClick={onClick}>Another metric</button>
                     </li>
-                    <li class="nav-overview" id="another-metric">
-                        <button onClick={onClick}>One more metric for fun</button>
+                    <li class="nav-overview">
+                        <button class="btn" id="one-more" onClick={onClick}>One more metric for fun</button>
                     </li>
                 </ul>
             </nav>
