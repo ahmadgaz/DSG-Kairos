8d5fb6cac7e592deeea419d4f58ca00a4580c8b0
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2022-12-14 23:50:29 -0800

    passed ageGroups data from server.py to Table

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,64 +1,79 @@
-import "./styles.css"
+import "./styles.css";
 
 const Table = () => {
-  return (
-    <div>
-      <table>
-        <tr>
-          <th class="column1" id="header1">DATE</th>
-          <th class="column2" id="header2">ATTENDEES</th>
-        </tr>
-        <tr>
-          <td class="column1">Jan</td>
-          <td class="column2">25</td>
-        </tr>
-        <tr>
-          <td class="column1">Feb</td>
-          <td class="column2">50</td>
-        </tr>
-        <tr>
-          <td class="column1">Mar</td>
-          <td class="column2">130</td>
-        </tr>
-        <tr>
-          <td class="column1">Apr</td>
-          <td class="column2">115</td>
-        </tr>
-        <tr>
-          <td class="column1">May</td>
-          <td class="column2">94</td>
-        </tr>
-        <tr>
-          <td class="column1">Jun</td>
-          <td class="column2">10</td>
-        </tr>
-        <tr>
-          <td class="column1">Jul</td>
-          <td class="column2">82</td>
-        </tr>
-        <tr>
-          <td class="column1">Aug</td>
-          <td class="column2">74</td>
-        </tr>
-        <tr>
-          <td class="column1">Sep</td>
-          <td class="column2">128</td>
-        </tr>
-        <tr>
-          <td class="column1">Oct</td>
-          <td class="column2">100</td>
-        </tr>
-        <tr>
-          <td class="column1">Nov</td>
-          <td class="column2">111</td>
-        </tr>
-        <tr>
-          <td class="column1">Dec</td>
-          <td class="column2">64</td>
-        </tr>
-      </table>
-    </div>
-  )
-}
+    const [ageGroups, setAgeGroups] = useState([{}]);
 
-export default Table
+    useEffect(() => {
+        fetch("/ageGroups")
+            .then((res) => res.json())
+            .then((data) => {
+                setAgeGroups(data);
+                console.log(data);
+            });
+    }, []);
+
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th class="column1" id="header1">
+                        DATE
+                    </th>
+                    <th class="column2" id="header2">
+                        ATTENDEES
+                    </th>
+                </tr>
+                <tr>
+                    <td class="column1">Jan</td>
+                    <td class="column2">25</td>
+                </tr>
+                <tr>
+                    <td class="column1">Feb</td>
+                    <td class="column2">50</td>
+                </tr>
+                <tr>
+                    <td class="column1">Mar</td>
+                    <td class="column2">130</td>
+                </tr>
+                <tr>
+                    <td class="column1">Apr</td>
+                    <td class="column2">115</td>
+                </tr>
+                <tr>
+                    <td class="column1">May</td>
+                    <td class="column2">94</td>
+                </tr>
+                <tr>
+                    <td class="column1">Jun</td>
+                    <td class="column2">10</td>
+                </tr>
+                <tr>
+                    <td class="column1">Jul</td>
+                    <td class="column2">82</td>
+                </tr>
+                <tr>
+                    <td class="column1">Aug</td>
+                    <td class="column2">74</td>
+                </tr>
+                <tr>
+                    <td class="column1">Sep</td>
+                    <td class="column2">128</td>
+                </tr>
+                <tr>
+                    <td class="column1">Oct</td>
+                    <td class="column2">100</td>
+                </tr>
+                <tr>
+                    <td class="column1">Nov</td>
+                    <td class="column2">111</td>
+                </tr>
+                <tr>
+                    <td class="column1">Dec</td>
+                    <td class="column2">64</td>
+                </tr>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
